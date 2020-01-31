47686100fd8f8e8a351f32c30fef7a5ea904d38b
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-01-31 16:36:35 +0100

    Add initial ConfigRepo api

---
+++ src/com/baloise/jenkinslibrary/containerdeploy/api/TeamConfigRepository.groovy
@@ -0,0 +1,20 @@
+package com.baloise.jenkinslibrary.containerdeploy.api
+
+def runSync(webhookChangeEventPayload) {
+    def organisation = webhookChangeEventPayload["repository"]["project"]["key"]
+    def repository = webhookChangeEventPayload["repository"]["slug"]
+    def refId = webhookChangeEventPayload["changes"][0]["refId"]
+    if ("refs/heads/master".equals(refId)) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '0f34a51f-334f-4ebe-a663-90e312fb32f6', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            return sh(script: """sudo podman run --rm -ti docker.io/niiku/gitopscli:v0.1.0 sync-apps \
+--username "${USERNAME}" \
+--password "${PASSWORD}" \
+--organisation "${organisation}" \
+--repository-name "${repository}" \
+--root-organisation "DPL" \
+--root-repository-name "apps-root-config" \
+--git-provider-url https://bitbucket.baloise.dev \
+""", returnStdout: true)
+        }
+    }
+}
