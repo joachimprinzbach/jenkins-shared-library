6488694ad2015bcd131ee115931273e4f241aef4
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-02-02 22:55:35 +0100

    Add createComment for PR

--- src/com/baloise/jenkinslibrary/git/api/PullRequest.groovy
+++ src/com/baloise/jenkinslibrary/git/api/PullRequest.groovy
@@ -1,7 +1,11 @@
 package com.baloise.jenkinslibrary.git.api
 
-def createComment(org, repo, prId, comment) {
-    return sh(script: """sudo podman run --rm -ti docker.io/baloiseincubator/gitopscli:latest create-pr-comment \
+def createComment(webhookChangeEventPayload, comment) {
+    def org = webhookChangeEventPayload["pullRequest"]["fromRef"]["repository"]["project"]["key"]
+    def repo = webhookChangeEventPayload["pullRequest"]["fromRef"]["repository"]["slug"]
+    def prId = webhookChangeEventPayload["pullRequest"]["id"]
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '0f34a51f-334f-4ebe-a663-90e312fb32f6', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+        return sh(script: """sudo podman run --rm -ti docker.io/baloiseincubator/gitopscli:latest create-pr-comment \
 --username "${USERNAME}" \
 --password "${PASSWORD}" \
 --organisation "${org}" \
@@ -10,4 +14,5 @@ def createComment(org, repo, prId, comment) {
 --text "${comment}" \
 --git-provider-url https://bitbucket.baloise.dev \
 """, returnStdout: true)
+    }
 }
